aab5cedff9eac62bafbaa65f2210d9b66aae0ec6
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-08-10 14:57:56 -0400

    added a funtioc wit the same name

--- src/test_functions.rs
+++ src/test_functions.rs
@@ -6,3 +6,11 @@ pub fn not_empty_test() {
     println!("not empty test");
     println!("added more chages");
 }
+
+pub struct Test;
+
+impl Test {
+    pub fn empty_test() {
+        println!("empty test");
+    }
+}
