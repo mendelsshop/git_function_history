c2748363e6da4a630701babfa98e7b170e70e37c
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-08-11 23:17:11 -0400

    added more dummy functions

--- src/test_functions.rs
+++ src/test_functions.rs
@@ -7,14 +7,17 @@ pub fn not_empty_test() {
     println!("{{ {} added more chages", "{");
 }
 
-pub struct Test;
+pub struct Test<a> {
+    pub contents: String,
+    pub history: Vec<a>,
+}
 
-impl Test {
-    pub fn empty_test() {
+impl<a>Test<a>  {
+    pub fn empty_test<'a>() {
         println!("empty test");
     }
 
-    pub fn  not_empty_test ( )  {
+    pub fn  not_empty_test( )  {
         println!("not empty test");
         println!("{{ {} added more chages", "{");
     }
@@ -24,7 +27,7 @@ impl Test {
         // }
     }
 
-    pub fn returns () -> String {
+    pub fn returns() -> String {
         String::from("fn empty_test() ")
     }
 }
@@ -33,4 +36,36 @@ pub trait super_trait {
     fn super_trait_method(&self);
 
     fn empty_test<T>() ->  String where T: super_trait;
+}
+
+impl <'a, t> super_trait for t {
+    fn super_trait_method (&self) {
+        println!("super trait method");}
+/*   dff
+gdg
+*/
+        fn empty_test<T>() ->  String where T: super_trait {
+            String::from("fn empty_test() ")
+        }
+    
+}
+
+
+trait Example {
+    const CONST_NO_DEFAULT: i32;
+    const CONST_WITH_DEFAULT: i32 = 99;
+    type TypeNoDefault;
+    fn method_without_default(&self);
+    fn method_with_default(&self) {}
+}
+
+trait Seq<T> {
+    fn len(&self) -> u32;
+    fn elt_at(&self, n: u32) -> T;
+    fn iter<F>(&self, f: F) where F: Fn(T);
+}
+trait super_trait_2 <T> where T: super_trait {
+    fn super_trait_method(&self) {
+        println!("super trait method");
+    }
 }
