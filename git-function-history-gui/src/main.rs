16aee9cdada30a4d061577c924c8efbf90d22407
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-08-22 00:30:31 -0400

    started working on gui

--- git-function-history-gui/src/main.rs
+++ git-function-history-gui/src/main.rs
@@ -1,3 +1,115 @@
+use eframe::egui::{TopBottomPanel, Visuals};
+use eframe::{
+    self,
+    egui::{self, Button, Context, Layout},
+    epaint::Vec2,
+    run_native,
+};
 fn main() {
-    println!("Hello, world!");
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.initial_window_size = Some(Vec2::new(800.0, 600.0));
+    run_native(
+        "Git Function History",
+        native_options,
+        Box::new(|cc| Box::new(MyEguiApp::new(cc))),
+    )
 }
+
+#[derive(Default)]
+struct MyEguiApp {
+    command: Command,
+    dark_theme: bool,
+    input_buffer: String,
+}
+impl MyEguiApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self {
+            dark_theme: true,
+            command: Command::Search,
+            input_buffer: String::new(),
+        }
+    }
+
+    pub(crate) fn render_top_panel(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
+        // define a TopBottomPanel widget
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            ui.add_space(10.);
+            egui::menu::bar(ui, |ui| {
+                // logo
+                ui.with_layout(Layout::left_to_right(eframe::emath::Align::Center), |_ui| {
+                });
+                // controls
+                ui.with_layout(Layout::right_to_left(eframe::emath::Align::Center), |ui| {
+                    let close_btn = ui.add(Button::new("❌"));
+                    let theme_btn = ui.add(Button::new("🌙"));
+                    if theme_btn.clicked() {
+                        self.dark_theme = !self.dark_theme;
+                    }
+                    if close_btn.clicked() {
+                        frame.close();
+                    }
+                });
+            });
+            ui.add_space(10.);
+        });
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        if self.dark_theme {
+            ctx.set_visuals(Visuals::dark());
+        } else {
+            ctx.set_visuals(Visuals::light());
+        }
+        egui::TopBottomPanel::bottom("status_bar").show(ctx, |_ui| {
+
+        });
+        self.render_top_panel(ctx, frame);
+        egui::TopBottomPanel::bottom("commnad_builder").show(ctx, |ui| {
+            egui::ComboBox::from_label("Select one!")
+                .selected_text(self.command.to_string())
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(&mut self.command, Command::Filter, "filter");
+                    ui.selectable_value(&mut self.command, Command::Search, "search");
+                    ui.selectable_value(&mut self.command, Command::List, "history");
+                });
+                // let text_input = ui.text_edit_singleline(&mut self.input_buffer);
+                // if text_input.lost_focus() && ui.input().key_pressed(egui::Key::Enter) {
+
+                // }
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |_ui| {});
+        });
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Command {
+    Filter,
+    List,
+    Search,
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Command::Search
+    }
+}
+
+impl std::fmt::Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Command::Filter => write!(f, "filter"),
+            Command::List => write!(f, "list"),
+            Command::Search => write!(f, "search"),
+        }
+    }
+}
+    
