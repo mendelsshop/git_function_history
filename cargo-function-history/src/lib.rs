199bb2c6b5844d3f3a0eb9073c7d6f7567417edd
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-09-04 13:49:23 -0400

    moved command thread code too lib.rs statrted working on cli arg parsing

--- cargo-function-history/src/lib.rs
+++ cargo-function-history/src/lib.rs
@@ -1,15 +1,11 @@
-use std::io::stdout;
-use std::rc::Rc;
-use std::{cell::RefCell, time::Duration};
+use std::{io::stdout, sync::mpsc::{Receiver, RecvTimeoutError, Sender}, thread, rc::Rc, cell::RefCell, time::Duration};
 
-use app::{state::AppState, App, AppReturn};
-
+use app::{state::AppState, App, AppReturn, CommandResult, ui::Status};
+use git_function_history::get_function_history;
 use eyre::Result;
-use inputs::events::Events;
-use inputs::InputEvent;
-use tui::backend::CrosstermBackend;
-use tui::Terminal;
-
+use inputs::{events::Events, InputEvent};
+use tui::{backend::CrosstermBackend, Terminal};
+use types::{HistoryFilter, FullCommand, FilterType, ListType};
 use crate::app::ui;
 
 pub mod app;
@@ -61,4 +57,224 @@ pub fn start_ui(app: Rc<RefCell<App>>) -> Result<()> {
     crossterm::terminal::disable_raw_mode()?;
 
     Ok(())
+}
+
+pub fn command_thread(rx_t: Receiver<FullCommand>, tx_t: Sender<(CommandResult, Status)>) {
+    thread::spawn(move || {
+        loop {
+            // TODO: change all the commented out println! to log
+            match rx_t.recv_timeout(Duration::from_millis(100)) {
+                Err(a) => {
+                    match a {
+                        RecvTimeoutError::Timeout => {
+                            // // println!("thread timeout");
+                        }
+                        RecvTimeoutError::Disconnected => {
+                            panic!("channel disconnected");
+                        }
+                    }
+                }
+                Ok(msg) => match msg {
+                    FullCommand::List(list_type) => {
+                        // println!("list");
+                        match list_type {
+                            ListType::Commits => {
+                                match git_function_history::get_git_commits() {
+                                    Ok(commits) => {
+                                        // println!("found {} commits", commits.len());
+                                        tx_t.send((
+                                            CommandResult::String(commits),
+                                            Status::Ok(Some("Found commits dates".to_string())),
+                                        ))
+                                        .unwrap();
+                                    }
+                                    Err(err) => {
+                                        tx_t.send((
+                                            CommandResult::None,
+                                            Status::Error(err.to_string()),
+                                        ))
+                                        .unwrap();
+                                    }
+                                };
+                            }
+                            ListType::Dates => {
+                                match git_function_history::get_git_dates() {
+                                    Ok(dates) => {
+                                        // println!("found {} dates", dates.len());
+                                        tx_t.send((
+                                            CommandResult::String(dates),
+                                            Status::Ok(Some("Found dates".to_string())),
+                                        ))
+                                        .unwrap();
+                                    }
+                                    Err(err) => {
+                                        tx_t.send((
+                                            CommandResult::None,
+                                            Status::Error(err.to_string()),
+                                        ))
+                                        .unwrap();
+                                    }
+                                };
+                            }
+                        }
+                    }
+                    FullCommand::Search(name, file, filter) => {
+                        // println!("Searching for {} in {:?}", name, file);
+                        match get_function_history(&name, file, filter) {
+                            Ok(functions) => {
+                                let hist_len = functions.history.len();
+                                let _commit_len = if hist_len > 0 {
+                                    functions.history[0].functions.len()
+                                } else {
+                                    0
+                                };
+                                // println!("Found functions",);
+                                tx_t.send((
+                                    CommandResult::History(
+                                        functions,
+                                        
+                                        
+                                    ),
+                                    Status::Ok(Some("Found functions".to_string())),
+                                ))
+                                .unwrap();
+                            }
+                            Err(err) => {
+                                tx_t.send((CommandResult::None, Status::Error(err.to_string())))
+                                    .unwrap();
+                            }
+                        };
+                    }
+                    FullCommand::Filter(t) => match t {
+                        FilterType::History(functions, history) => match functions {
+                            HistoryFilter::Date(date) => {
+                                match history.get_by_date(&date) {
+                                    Some(functions) => {
+                                        tx_t.send((
+                                            CommandResult::Commit(
+                                                functions.clone(),
+                                            ),
+                                            Status::Ok(Some("Found functions".to_string())),
+                                        ))
+                                        .unwrap();
+                                    }
+                                    None => {
+                                        tx_t.send((
+                                            CommandResult::None,
+                                            Status::Error(format!(
+                                                "No functions found for date {}",
+                                                date
+                                            )),
+                                        ))
+                                        .unwrap();
+                                    }
+                                };
+                            }
+                            HistoryFilter::CommitId(commit) => {
+                                match history.get_by_commit_id(&commit) {
+                                    Some(functions) => {
+                                        tx_t.send((
+                                            CommandResult::Commit(
+                                                functions.clone(),
+                                                
+                                            ),
+                                            Status::Ok(Some("Found functions".to_string())),
+                                        ))
+                                        .unwrap();
+                                    }
+                                    None => {
+                                        tx_t.send((
+                                            CommandResult::None,
+                                            Status::Error(format!(
+                                                "No functions found for commit {}",
+                                                commit
+                                            )),
+                                        ))
+                                        .unwrap();
+                                    }
+                                };
+                            }
+                            HistoryFilter::DateRange(frst, scd) => {
+                                let t = history.get_date_range(&frst, &scd);
+                                let hist_len = t.history.len();
+                                let _commit_len = if hist_len > 0 {
+                                    t.history[0].functions.len()
+                                } else {
+                                    0
+                                };
+                                // println!("Found functions",);
+                                tx_t.send((
+                                    CommandResult::History(
+                                        t,
+                                        
+                                        
+                                    ),
+                                    Status::Ok(Some("Found functions".to_string())),
+                                ))
+                                .unwrap();
+                            }
+                            HistoryFilter::FunctionInBlock(block) => {
+                                let t = history.get_all_functions_in_block(block);
+                                let hist_len = t.history.len();
+                                let _commit_len = if hist_len > 0 {
+                                    t.history[0].functions.len()
+                                } else {
+                                    0
+                                };
+                                // println!("Found functions",);
+                                tx_t.send((
+                                    CommandResult::History(
+                                        t,
+                                        
+                                        
+                                    ),
+                                    Status::Ok(Some("Found functions".to_string())),
+                                ))
+                                .unwrap();
+                            }
+                            HistoryFilter::FunctionInLines(line1, line2) => {
+                                let t = history.get_all_functions_line(line1, line2);
+                                let hist_len = t.history.len();
+                                let _commit_len = if hist_len > 0 {
+                                    t.history[0].functions.len()
+                                } else {
+                                    0
+                                };
+                                // println!("Found functions",);
+                                tx_t.send((
+                                    CommandResult::History(
+                                        t,
+                                        
+                                        
+                                    ),
+                                    Status::Ok(Some("Found functions".to_string())),
+                                ))
+                                .unwrap();
+                            }
+                            HistoryFilter::FunctionInFunction(function) => {
+                                let t = history.get_all_function_with_parent(&function);
+                                let hist_len = t.history.len();
+                                let _commit_len = if hist_len > 0 {
+                                    t.history[0].functions.len()
+                                } else {
+                                    0
+                                };
+                                // println!("Found functions",);
+                                tx_t.send((
+                                    CommandResult::History(
+                                        t,
+                                        
+                                        
+                                    ),
+                                    Status::Ok(Some("Found functions".to_string())),
+                                ))
+                                .unwrap();
+                            }
+                        },
+                        FilterType::CommitOrFile(_commit, _t) => {}
+                    },
+                },
+            }
+        }
+    });
 }
