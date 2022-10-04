8bb603b968dcbb0dae13379521b22cb3e609fb16
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-10-04 09:26:57 -0400

    working on c parsing

---
+++ git-function-history-lib/src/test_functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void test_function(void);
+
+void test_function2(void)
+{
+    printf("Hello World!" );
+}
+
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
+
+void test_function(void)
+{
+    printf("Hello World!");
+}
