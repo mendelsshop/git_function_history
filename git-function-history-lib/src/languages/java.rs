f8bfc8321732da0e68e7bf39f1180f3b44005ff4
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-10-23 08:06:05 -0400

    more javas stuff

--- git-function-history-lib/src/languages/java.rs
+++ git-function-history-lib/src/languages/java.rs
@@ -1,5 +1,7 @@
 use std::fmt;
 
+use javaparser::parse::tree::CompilationUnit;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct JavaFunction {
     pub name: String,
@@ -47,12 +49,29 @@ pub struct JavaClass {
 pub(crate) fn find_function_in_file(
     file_contents: &str,
     name: &str,
-) -> Result<JavaFunction, String> {
+) -> Result<Vec<JavaFunction>, String> {
     let file = javaparser::parse::apply(file_contents, "<stdin>").map_err(|_| "Parse error")?;
-    let parsed = file.unit.clone();
-    println!("{:#?}", file);
+    let parsed = file.unit.clone().items;
+    println!("{:#?}", parsed);
     Err("Not implemented".to_string())
 }
+
+fn extract_methods_from_compilation_unit(
+    unit: &CompilationUnit<'_>,
+    name: &str,
+) -> Result<Vec<JavaFunction>, String> {
+    let mut methods = Vec::new();
+    for item in &unit.items {
+        match item {
+            javaparser::parse::tree::CompilationUnitItem::Class(_) => todo!(),
+            javaparser::parse::tree::CompilationUnitItem::Interface(_) => todo!(),
+            javaparser::parse::tree::CompilationUnitItem::Annotation(_) => todo!(),
+            javaparser::parse::tree::CompilationUnitItem::Enum(_) => todo!(),
+        }
+    }
+    Ok(methods)
+    }
+
 
 #[cfg(test)]
 mod java_test {
