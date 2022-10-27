ed6fafeb9956e2c8e9160aeebd28eb556a9332b3
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-10-26 21:16:19 -0400

    idk what im doing but we can now semi find methods in java

--- git-function-history-lib/src/languages/java.rs
+++ git-function-history-lib/src/languages/java.rs
@@ -60,25 +60,162 @@ pub(crate) fn find_function_in_file(
 ) -> Result<Vec<JavaFunction>, String> {
     let file = javaparser::parse::apply(file_contents, "<stdin>").map_err(|_| "Parse error")?;
     let parsed = file.unit.clone().items;
-    println!("{:#?}", parsed);
+    // println!("{:#?}", parsed);
     let mut functions = Vec::new();
     for unit in parsed {
-        extract_methods_from_compilation_unit(&unit, name).map(|f| functions.push(f))?;
+        extract_methods_from_compilation_unit(&unit, name).map(|f| functions.extend(f))?;
     }
-    Err("Not implemented".to_string())
+    Ok(functions)
 }
 
 fn extract_methods_from_compilation_unit(
     unit: &CompilationUnitItem<'_>,
     name: &str,
 ) -> Result<Vec<JavaFunction>, String> {
+    // recursively search for items with type Method
+
     let mut methods = Vec::new();
 
         match unit {
-        javaparser::parse::tree::CompilationUnitItem::Class(_) => todo!(),
-        javaparser::parse::tree::CompilationUnitItem::Interface(_) => todo!(),
-        javaparser::parse::tree::CompilationUnitItem::Annotation(_) => todo!(),
-        javaparser::parse::tree::CompilationUnitItem::Enum(_) => todo!(),
+        javaparser::parse::tree::CompilationUnitItem::Class(class) => {
+            let mut class_methods = Vec::new();
+            for item in &class.body.items {
+                extract_methods_from_class_item(item, name).map(|f| class_methods.extend(f))?;
+            }
+            methods.extend(class_methods);
+
+        }
+        javaparser::parse::tree::CompilationUnitItem::Interface(interface) => {
+            let mut interface_methods = Vec::new();
+            for item in &interface.body.items {
+                extract_methods_from_class_item(item, name).map(|f| interface_methods.extend(f))?;
+            }
+            methods.extend(interface_methods);
+        },
+        javaparser::parse::tree::CompilationUnitItem::Enum(enum_) => {
+            let mut enum_methods = Vec::new();
+            if let Some(enum_body) = &enum_.body_opt {
+                for item in &enum_body.items {
+                    extract_methods_from_class_item(item, name).map(|f| enum_methods.extend(f))?;
+                }
+            }
+            methods.extend(enum_methods);
+        },
+        javaparser::parse::tree::CompilationUnitItem::Annotation(annotation) => {
+            let mut annotation_methods = Vec::new();
+            for item in &annotation.body.items {
+                extract_methods_from_annotation_item(item, name).map(|f| annotation_methods.extend(f))?;
+            }
+            methods.extend(annotation_methods);
+        
+        },
+    }
+    Ok(methods)
+}
+
+fn extract_methods_from_class_item(
+    item: &javaparser::parse::tree::ClassBodyItem<'_>,
+    name: &str,
+) -> Result<Vec<JavaFunction>, String> {
+    let mut methods = Vec::new();
+    match item {
+        javaparser::parse::tree::ClassBodyItem::Method(method) => {
+            // println!("{:#?}", method);
+            if method.name.fragment == name {
+                let args = vec![];
+                // method
+                    // .parameters
+                    // .iter()
+                    // .map(|p| p.name.to_string())
+                    // .collect::<Vec<String>>();
+                // let body = method.body.to_string();
+                // let lines = (method.line, method.line + body.lines().count());
+                let class = Vec::new();
+                methods.push(JavaFunction::new(
+                    "test".to_string(),
+                    (0, 0),
+                    args,
+                    "test".to_string(),
+                    class,
+                ));
+            }
+        }
+        javaparser::parse::tree::ClassBodyItem::Class(class) => {
+            let mut class_methods = Vec::new();
+            for item in &class.body.items {
+                extract_methods_from_class_item(item, name).map(|f| class_methods.extend(f))?;
+            }
+            methods.extend(class_methods);
+        }
+        javaparser::parse::tree::ClassBodyItem::Interface(interface) => {
+            let mut interface_methods = Vec::new();
+            for item in &interface.body.items {
+                extract_methods_from_class_item(item, name).map(|f| interface_methods.extend(f))?;
+            }
+            methods.extend(interface_methods);
+        }
+        javaparser::parse::tree::ClassBodyItem::Enum(enum_) => {
+            let mut enum_methods = Vec::new();
+            if let Some(enum_body) = &enum_.body_opt {
+                for item in &enum_body.items {
+                    extract_methods_from_class_item(item, name).map(|f| enum_methods.extend(f))?;
+                }
+            }
+            methods.extend(enum_methods);
+        }
+        javaparser::parse::tree::ClassBodyItem::Annotation(annotation) => {
+            let mut annotation_methods = Vec::new();
+            for item in &annotation.body.items {
+                extract_methods_from_annotation_item(item, name).map(|f| annotation_methods.extend(f))?;
+            }
+            methods.extend(annotation_methods);
+        }
+        javaparser::parse::tree::ClassBodyItem::Constructor(constructor) => {}
+        javaparser::parse::tree::ClassBodyItem::FieldDeclarators(field_declarators) => {}
+        javaparser::parse::tree::ClassBodyItem::StaticInitializer(static_initializer) => {}
+
+    }
+    Ok(methods)
+}
+
+fn extract_methods_from_annotation_item(
+    item: &javaparser::parse::tree::AnnotationBodyItem<'_>,
+    name: &str,
+) -> Result<Vec<JavaFunction>, String> {
+    let mut methods = Vec::new();
+    match item {
+        javaparser::parse::tree::AnnotationBodyItem::Annotation(annotation) => {
+            let mut annotation_methods = Vec::new();
+            for item in &annotation.body.items {
+                extract_methods_from_annotation_item(item, name).map(|f| annotation_methods.extend(f))?;
+            }
+            methods.extend(annotation_methods);
+        }
+        javaparser::parse::tree::AnnotationBodyItem::FieldDeclarators(field_declarators) => {}
+        javaparser::parse::tree::AnnotationBodyItem::Class(class) => {
+            let mut class_methods = Vec::new();
+            for item in &class.body.items {
+                extract_methods_from_class_item(item, name).map(|f| class_methods.extend(f))?;
+            }
+            methods.extend(class_methods);
+        }
+        javaparser::parse::tree::AnnotationBodyItem::Interface(interface) => {
+            let mut interface_methods = Vec::new();
+            for item in &interface.body.items {
+                extract_methods_from_class_item(item, name).map(|f| interface_methods.extend(f))?;
+            }
+            methods.extend(interface_methods);
+        }
+        javaparser::parse::tree::AnnotationBodyItem::Enum(enum_) => {
+            let mut enum_methods = Vec::new();
+            if let Some(enum_body) = &enum_.body_opt {
+                for item in &enum_body.items {
+                    extract_methods_from_class_item(item, name).map(|f| enum_methods.extend(f))?;
+                }
+            }
+            methods.extend(enum_methods);
+        }
+        javaparser::parse::tree::AnnotationBodyItem::Param(param) => {}
     }
     Ok(methods)
 }
@@ -90,6 +227,7 @@ mod java_test {
     #[test]
     fn java() {
         let file_contents = r#"
+        @Company
             public class Test {
                 public static void main(String[] args) {
                     System.out.println("Hello, World");
@@ -97,6 +235,7 @@ mod java_test {
             }
         "#;
         let function = find_function_in_file(file_contents, "main").unwrap();
+        println!("{:#?}", function);
     }
 
     #[test]
